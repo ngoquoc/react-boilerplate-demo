16b6815d3d75c417b6e295a0796527be3931fff8
Steve Tran <anh.tran@2clicksolutions.com>
2018-07-12 11:37:04 +0700

    auto assign development env for unspecified env

--- config/env.js
+++ config/env.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const paths = require('./paths');
@@ -7,15 +5,15 @@ const paths = require('./paths');
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
 
-const NODE_ENV = process.env.NODE_ENV;
-if (!NODE_ENV) {
-  throw new Error(
-    'The NODE_ENV environment variable is required but was not specified.'
-  );
-}
+const NODE_ENV = process.env.NODE_ENV || 'development';
+// if (!NODE_ENV) {
+//   throw new Error(
+//     'The NODE_ENV environment variable is required but was not specified.'
+//   );
+// }
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-var dotenvFiles = [
+const dotenvFiles = [
   `${paths.dotenv}.${NODE_ENV}.local`,
   `${paths.dotenv}.${NODE_ENV}`,
   // Don't include `.env.local` for `test` environment
@@ -30,13 +28,11 @@ var dotenvFiles = [
 // that have already been set.  Variable expansion is supported in .env files.
 // https://github.com/motdotla/dotenv
 // https://github.com/motdotla/dotenv-expand
-dotenvFiles.forEach(dotenvFile => {
+dotenvFiles.forEach((dotenvFile) => {
   if (fs.existsSync(dotenvFile)) {
-    require('dotenv-expand')(
-      require('dotenv').config({
-        path: dotenvFile,
-      })
-    );
+    require('dotenv-expand')(require('dotenv').config({
+      path: dotenvFile,
+    }));
   }
 });
 
