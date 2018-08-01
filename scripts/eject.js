f966a4c16e18fc07facd6629f96a04577a974cc9
Steve Tran <anh.tran@2clicksolutions.com>
2018-08-01 16:33:03 +0700

    implement basic eject processes handlers

--- scripts/eject.js
+++ scripts/eject.js
@@ -1,4 +1,12 @@
-// process args
+const { log } = require('jsuti');
+// configs
+/**
+ * final args object
+ */
+const args = {};
+/**
+ * possible args
+ */
 const possibleArgs = [
   // App's name
   {
@@ -77,76 +85,293 @@ const possibleArgs = [
     default: undefined,
   },
 ];
-const getArg = key =>
-  possibleArgs.find(({ arg, abbr }) => key === arg || key === abbr);
-
-const possibleArgsKeys = [...possibleArgs.map(({ arg, abbr }) => [arg, abbr])];
-
-const args = {};
-// pair params and values
-process.argv.forEach((key, index, keys) => {
-  const paramKeyRegex = /^-{1,2}/;
-  if (!paramKeyRegex.test(key)) {
-    // key is param's value
-    return;
-  }
-  // key is param's name
-  const arg = getArg(key);
-  if (!arg) {
-    const suggestion = possibleArgsKeys.find(arg => arg.contains(key));
-    throw new Error(`Argument ${key} is invalid!`.concat(!suggestion ? '' : `Did you mean "${suggestion}"?`));
-  }
-  const { name, default: defaultValue } = arg;
-  const valueIndex = index + 1;
-  if (!keys[valueIndex] || paramKeyRegex.test(keys[valueIndex])) {
-    // value is not available
-    if (typeof defaultValue === 'undefined') {
-      throw new Error(`Argument ${key}'s value must be specified!`);
-    }
-    args[name] = defaultValue;
-    return;
-  }
-  args[name] = keys[valueIndex];
-});
-
 /**
- * Get current value or default value of a param
- * @param key {string} param's key
+ * Steps name constants
  */
-const valueOrDefault = (key, arg) => {
-  if (typeof args[key] !== 'function') {
-    if (typeof args[key] !== 'undefined') {
-      return args[key];
-    }
-    if (typeof arg.default !== 'function') {
-      return arg.default;
-    }
-    return arg.default(args);
-  }
-  // default value is a function
-  return args[key](args);
+const STEPS = {
+  PREPARE: 'prepare',
+  EJECT_PACKAGE_JSON: 'eject package.json',
+  EJECT_CONFIGS: 'eject configs',
+  EJECT_CONFIG_DEV_JSON: 'eject config.dev.json',
+  EJECT_CONFIG_PROD_JSON: 'eject config.prod.json',
+  EJECT_PUBLIC_ASSETS: 'eject public assets',
+  EJECT_ASSETS: 'eject assets',
+  EJECT_FAVICON: 'eject favicon',
+  EJECT_INDEX_HTML: 'eject index.html',
+  EJECT_MANIFEST_JSON: 'eject manifest.json',
+  EJECT_SRC: 'eject src',
+  EJECT_APP_JS: 'eject app.js',
+  EJECT_CONFIG_JS: 'eject config.js',
+  EJECT_COMPONENTS: 'eject components',
+  EJECT_CONTAINERS: 'eject containers',
+  EJECT_HOCS: 'eject HOCs',
+  EJECT_INTL: 'eject intl',
+  EJECT_MOCK_DATA: 'eject mockData',
 };
+/**
+ * eject steps
+ */
+const ejectSteps = [
+  // prepare
+  {
+    name: STEPS.PREPARE,
+    exec: (args, step) => {
+      const getArg = key =>
+        possibleArgs.find(({ arg, abbr }) => key === arg || key === abbr);
 
-// finalize args
-possibleArgs.forEach((arg) => {
-  args[arg.name] = valueOrDefault(arg.name, arg);
-});
+      const possibleArgsKeys = [
+        ...possibleArgs.map(({ arg, abbr }) => [arg, abbr]),
+      ];
 
-console.log(args);
-// eject package.json
-// eject configs
-// / eject config.dev.json
-// / eject config.prod.json
-// eject public assets
-// / eject assets
-// / eject favicon
-// / eject index.html
-// /eject manifest.json
-// eject src
-// /eject app.js
-// / eject config.js
-// / eject components
-// /eject containers
-// /eject HOCs
-// /eject intl
-// /eject mockData
+      // pair params and values
+      process.argv.forEach((key, index, keys) => {
+        const paramKeyRegex = /^-{1,2}/;
+        if (!paramKeyRegex.test(key)) {
+          // key is param's value
+          return;
+        }
+        // key is param's name
+        const arg = getArg(key);
+        if (!arg) {
+          const suggestion = possibleArgsKeys.find(arg => arg.contains(key));
+          throw new Error(`Argument ${key} is invalid!`.concat(!suggestion ? '' : `Did you mean "${suggestion}"?`));
+        }
+        const { name, default: defaultValue } = arg;
+        const valueIndex = index + 1;
+        if (!keys[valueIndex] || paramKeyRegex.test(keys[valueIndex])) {
+          // value is not available
+          if (typeof defaultValue === 'undefined') {
+            throw new Error(`Argument ${key}'s value must be specified!`);
+          }
+          args[name] = defaultValue;
+          return;
+        }
+        args[name] = keys[valueIndex];
+      });
+
+      /**
+       * Get current value or default value of a param
+       * @param key {string} param's key
+       */
+      const valueOrDefault = (key, arg) => {
+        if (typeof args[key] !== 'function') {
+          if (typeof args[key] !== 'undefined') {
+            return args[key];
+          }
+          if (typeof arg.default !== 'function') {
+            return arg.default;
+          }
+          return arg.default(args);
+        }
+        // default value is a function
+        return args[key](args);
+      };
+
+      // finalize args
+      possibleArgs.forEach((arg) => {
+        args[arg.name] = valueOrDefault(arg.name, arg);
+      });
+    },
+  },
+  // eject package.json
+  {
+    name: STEPS.EJECT_PACKAGE_JSON,
+    exec: async (args, step) => {
+      const npmPackages = require('../package.json');
+      const outputPackages = {
+        ...npmPackages,
+        name: args.package,
+        homepage: args.home,
+        private: args.private,
+      };
+    },
+    undo: async (args, step) => {},
+  },
+  // eject configs
+  {
+    name: STEPS.EJECT_CONFIGS,
+    childProcesses: [STEPS.EJECT_CONFIG_DEV_JSON, STEPS.EJECT_CONFIG_PROD_JSON],
+  },
+  // / eject config.dev.json
+  {
+    name: STEPS.EJECT_CONFIG_DEV_JSON,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // / eject config.prod.json
+  {
+    name: STEPS.EJECT_CONFIG_PROD_JSON,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // eject public assets
+  {
+    name: STEPS.EJECT_PUBLIC_ASSETS,
+    childProcesses: [
+      STEPS.EJECT_ASSETS,
+      STEPS.EJECT_FAVICON,
+      STEPS.EJECT_INDEX_HTML,
+      STEPS.EJECT_MANIFEST_JSON,
+    ],
+  },
+  // / eject assets
+  {
+    name: STEPS.EJECT_ASSETS,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // / eject favicon
+  {
+    name: STEPS.EJECT_FAVICON,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // / eject index.html
+  {
+    name: STEPS.EJECT_INDEX_HTML,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // /eject manifest.json
+  {
+    name: STEPS.EJECT_MANIFEST_JSON,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // eject src
+  {
+    name: STEPS.EJECT_SRC,
+    childProcesses: [
+      STEPS.EJECT_APP_JS,
+      STEPS.EJECT_CONFIG_JS,
+      STEPS.EJECT_COMPONENTS,
+      STEPS.EJECT_CONTAINERS,
+      STEPS.EJECT_HOCS,
+      STEPS.EJECT_INTL,
+      STEPS.EJECT_MOCK_DATA,
+    ],
+  },
+  // /eject app.js
+  {
+    name: STEPS.EJECT_APP_JS,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // / eject config.js
+  {
+    name: STEPS.EJECT_CONFIG_JS,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // / eject components
+  {
+    name: STEPS.EJECT_COMPONENTS,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // /eject containers
+  {
+    name: STEPS.EJECT_CONTAINERS,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // /eject HOCs
+  {
+    name: STEPS.EJECT_HOCS,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // /eject intl
+  {
+    name: STEPS.EJECT_INTL,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+  // /eject mockData
+  {
+    name: STEPS.EJECT_MOCK_DATA,
+    exec: async (args, step) => {},
+    undo: async (args, step) => {},
+  },
+];
+/**
+ * exec all eject steps
+ * @param {*} ejectStep
+ */
+const execStep = async (step, index, steps) => {
+  const {
+    name, exec, undo, childProcesses, parent, isExecuted,
+  } = step;
+  if (isExecuted) {
+    return step.executed;
+  }
+  !step && log(name).write();
+  step.isExecuted = true;
+  const exectable = Boolean(exec);
+  log(`<green [Step ${name}]/> <yellow Step /><cyan ${name}/><yellow  started />`).write();
+  const isAsync = exec && exec.constructor.name === 'AsyncFunction';
+  if (exec && exec.constructor.name === 'AsyncFunction') {
+    let checkingIndex = index;
+    let previousStep = steps[checkingIndex - 1];
+    // find closest previous sync step
+    while (
+      previousStep &&
+      !previousStep.isExecuted &&
+      ((previousStep.parent && parent) || (!previousStep.parent && !parent)) &&
+      (!previousStep.exec ||
+        !previousStep.childProcesses ||
+        !previousStep.childProcesses.length ||
+        previousStep.exec.constructor.name === 'AsyncFunction')
+    ) {
+      checkingIndex--;
+      previousStep = steps[checkingIndex];
+    }
+    // wait until closest previous sync step done
+    if (steps[checkingIndex + 1] && steps[checkingIndex + 1].executed) {
+      log(`<green [Step ${name}]/> <yellow Waiting for /><cyan ${
+        steps[checkingIndex + 1].name
+      }/><yellow until it is done... />`).write();
+      await steps[checkingIndex + 1].executed;
+      log(`<green [Step ${name}]/> <grey Step /><cyan ${
+        steps[checkingIndex + 1].name
+      }/><grey  was done. />`).write();
+    }
+  }
+  // process childProcesses
+  if (childProcesses && childProcesses.length) {
+    const execChildProcess = (childName, index, steps) => {
+      const childStep = ejectSteps.find(({ name }) => name === childName);
+      childStep.parent = step;
+      return execStep(childStep, index, steps);
+    };
+    if (!exectable) {
+      log(`<green [Step ${name}]/> <yellow Waiting child processes until they are done... />`).write();
+      step.executed = Promise.all(childProcesses.map(execChildProcess)).then((result) => {
+        log(`<green [Step ${name}]/> <grey Child processes were done. />`).write();
+        log(`<green [Step ${name}]/> <grey Step /><cyan ${name}/><grey  executed. />`).write();
+        return result;
+      });
+
+      return step.executed;
+    }
+    log(`<green [Step ${name}]/> <yellow Waiting child processes until they are done... />`).write();
+    await Promise.all(childProcesses.map(childProcesses.map(execChildProcess)));
+    log(`<green [Step ${name}]/> <grey Child processes were done. />`).write();
+  }
+  if (exectable) {
+    log(`<green [Step ${name}]/> <yellow Step /><cyan ${name}/><yellow  Executing... />`).write();
+    step.executed = exec(args, step);
+    if (!isAsync) {
+      log(`<green [Step ${name}]/> <grey Step /><cyan ${name}/><grey  executed. />`).write();
+      return step.executed;
+    }
+    return step.executed.then((result) => {
+      log(`<green [Step ${name}]/> <grey Step /><cyan ${name}/><grey  executed. />`).write();
+      return result;
+    });
+  }
+  log(`<green [Step ${name}]/> <grey Step /><cyan ${name}/><grey  executed. />`).write();
+  return undefined;
+};
+// exec all eject steps
+ejectSteps.forEach(execStep);
